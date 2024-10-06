1eb6624b06c3d6012c60898eaf185162067b96ef
subodh-palayekar <subodhpalayekar651@gmail.com>
2024-10-06 14:59:04 +0530

    ytdl dependancy removed and new @distube/ytdl-core added

--- app/api/route.js
+++ app/api/route.js
@@ -1,20 +1,20 @@
-import { NextResponse } from "next/server";
-import { metadata } from "../layout";
-const ytdl = require("ytdl-core");
+import { NextResponse } from 'next/server';
+
+const ytdl = require('@distube/ytdl-core');
 
 export async function POST(request) {
   const reqBody = await request.json();
   const videoLink = reqBody.videoLink;
   console.log(videoLink);
 
-  const videoId =  ytdl.getVideoID(videoLink);
+  const videoId = ytdl.getVideoID(videoLink);
 
   const videoInfo = await ytdl.getInfo(videoId);
 
   const metaData = {
-    embed : videoInfo.videoDetails.embed,
-    title : videoInfo.videoDetails.title
-  }
+    embed: videoInfo.videoDetails.embed,
+    title: videoInfo.videoDetails.title,
+  };
 
   const filteredResponse = videoInfo.formats
     .filter((obj) => obj.url && obj.mimeType)
@@ -26,15 +26,14 @@ export async function POST(request) {
         hasAudio,
         hasVideo,
         sizeMB: (parseInt(contentLength, 10) / (1024 * 1024)).toFixed(2),
-        contentLength
+        contentLength,
       })
     );
 
-
-    const finalData = {
-      metaData,
-      videoData : filteredResponse
-    }
+  const finalData = {
+    metaData,
+    videoData: filteredResponse,
+  };
 
   return NextResponse.json(finalData);
 }
